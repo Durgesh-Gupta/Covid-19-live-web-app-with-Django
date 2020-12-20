2dba6271a0c4cbe573b2814beea8cf8f88eb7da2
durgesh gupta <durgesh10gupta@gmail.com>
2020-12-20 10:24:08 +0530

    Add Rapid API

--- CovidProject/MyApp/views.py
+++ CovidProject/MyApp/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render,HttpResponse
+import requests
+import json
+url = "https://covid-193.p.rapidapi.com/statistics"
 
+headers = {
+    'x-rapidapi-key': "b7ef0e1730msh15273a09ee3fdedp1e34b5jsnf8250eec58a4",
+    'x-rapidapi-host': "covid-193.p.rapidapi.com"
+    }
+
+response = requests.request("GET", url, headers=headers).json()
+
+# print(response.text)
 # Create your views here.
 def home(request):
-    return render(request,'index.html')
+    noofresults=int(response['results'])
+    lst=[]
+    for x in range(0,noofresults):
+        lst.append(response['response'][x]['country'])
+    d={'lst':lst}
+    return render(request,'index.html',d)
+
