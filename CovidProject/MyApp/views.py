a021fc215effc53d51744b1bddb17007a58445ff
durgesh gupta <durgesh10gupta@gmail.com>
2020-12-20 11:55:03 +0530

    Adding site Design

--- CovidProject/MyApp/views.py
+++ CovidProject/MyApp/views.py
@@ -13,10 +13,28 @@ response = requests.request("GET", url, headers=headers).json()
 # print(response.text)
 # Create your views here.
 def home(request):
-    noofresults=int(response['results'])
     lst=[]
+    noofresults=int(response['results'])
+
     for x in range(0,noofresults):
         lst.append(response['response'][x]['country'])
+
+    if request.method=='POST':
+        scountry=request.POST['selectedcountry']
+        print(scountry)
+        noofresults=int(response['results'])
+        for x in range(0,noofresults):
+            if scountry==response['response'][x]['country']:
+                active=response['response'][x]['cases']['active']
+                new=response['response'][x]['cases']['new']
+                critical=response['response'][x]['cases']['critical']
+                recovered=response['response'][x]['cases']['recovered']
+                total=response['response'][x]['cases']['total']
+                deaths=int(total)-int(active)-int(recovered)
+        d={'lst':lst,'scountry':scountry,'new':new,'active':active,'recovered':recovered,'critical':critical,'total':total,'deaths':deaths}
+        return render(request,'index.html',d)
+
+    
     d={'lst':lst}
     return render(request,'index.html',d)
 
